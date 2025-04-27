7d689112b2e8816f02329d55d9103a77c75aebb7
sumieren <aygoes@gmail.com>
2025-04-27 11:32:57 +0200

    added player draw

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 import pygame
 
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -17,12 +18,16 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill(0)
+
+        player.draw(screen)
 
         #draw the screen and pause until 1/60th of a second
         pygame.display.flip()
